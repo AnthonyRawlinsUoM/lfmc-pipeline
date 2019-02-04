0081ef75dba97ebce6fd83ab9a6cf08ab3323705
AnthonyRawlinsUoM <FuelMoisture2017>
2019-02-04 16:07:54 +1100

    version 1.0.0

--- bump.sh
+++ bump.sh
@@ -7,7 +7,7 @@ IMAGE=lfmc-pipeline
 git pull
 
 # bump version
-docker run --rm -v "$PWD":/app treeder/bump bump
+docker run --rm -v "$PWD":/app treeder/bump minor
 version=`cat VERSION`
 echo "version: $version"
 
